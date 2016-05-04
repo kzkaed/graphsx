a289d8909c38a41c5f1b710e8d187b658da478cf
kzkaed <tree@scutigera.com>
2016-05-04 09:03:34 -0500

    Project setup for graphs

--- project.clj
+++ project.clj
@@ -12,6 +12,14 @@
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/tools.reader "0.7.10"]]
+  :profiles {:dev {:main graphsx.core
+                   :dependencies [[org.clojure/tools.namespace "0.2.8"]
+                                  [speclj "3.1.0"]]
+                   :source-paths ["dev"]}
+             :test {:main graphsx.core
+                    :dependencies [[org.clojars.trptcolin/blackwater "0.0.9.2"]]}
+             }
+
   :aliases {"spec"
             ["with-profile" "+test" "spec"]
             "test"
@@ -20,7 +28,5 @@
             ["with-profile" "+test" "spec" "--tag" "integration"]}
   :plugins [[speclj "3.1.0"]
             [ragtime/ragtime.lein "0.3.6"]]
-
-
-  :source-paths ["src/clj"]
-  :test-paths ["spec/clj"])
+  :source-paths ["src"]
+  :test-paths ["spec"])
--- spec/graphsx/core_spec.clj
+++ spec/graphsx/core_spec.clj
@@ -1,7 +1,8 @@
 (ns graphsx.core-spec
-  (:require [graphsx.core :refer :all]
+  (:require [graphsx.core :as sut]
             [speclj.core :refer :all]))
 
-(describe "describe"
-  (context "context"
-    (it "spec desc")))
+(describe "describe the start of graphs"
+  (context "starting foo"
+    (it "just returns \"I don't do a whole lot.\""
+      (should="I don't do a whole lot." (sut/foo)))))
