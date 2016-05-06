3a899ccd5d44b589cabd3893fad11042fd53a98d
kzkaed <tree@scutigera.com>
2016-05-06 06:44:46 -0500

    vis

--- src/graphsx/core.clj
+++ src/graphsx/core.clj
@@ -2,3 +2,16 @@
 
 (defn foo []
   "I don't do a whole lot.")
+
+(defn read-csv [file]
+  (with-open [in-file (io/reader file)]
+    (doall
+      (csv/read-csv in-file))))
+
+(def edge-data (rest (read-csv "resources/data/stormofswords.csv")))
+
+(def graph
+  (reduce
+    (fn [g [source target weight]] (add-edge g source target))
+    (empty-graph)
+    edge-data))
